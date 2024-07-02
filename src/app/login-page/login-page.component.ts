ab47cae61e2f6b4d26305b0bc5b5e46f799312e2
Raghav Singh <raghavsingh@Raghavs-MacBook-Pro.local>
2024-07-02 08:50:19 +0530

    file

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  goToSignUp(): void {
+    this.router.navigate(['/signup']);
+  }
 }
+
